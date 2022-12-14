e18cfb160efc601856e4ab1213231bbfe6f9ff0a
mrsibatuaras <sibatuarachristian@gmail.com>
2022-12-15 02:12:40 +0700

    revisi

--- app.py
+++ app.py
@@ -312,35 +312,12 @@ def dreamTeam():
 
 key = "tiantampan"
 
-# ePass --> enteredPassword
-# cPass --> currentPassword
 def encodeStr(ePass):
   hashed_password = bcrypt.hashpw((key+ePass).encode("utf-8"), bcrypt.gensalt())
   return hashed_password
 
 def verifyUser(ePass, cPass):
   return bcrypt.checkpw((key+ePass).encode("utf-8"), cPass.encode("utf-8"))
-
-def jsonFormatArray(cursor):
-  headers = [x[0] for x in cursor.description]
-  data = cursor.fetchall()
-
-  res = []
-
-  for item in data:
-    res.append(dict(zip(headers, item)))
-
-  return res
-
-def jsonFormat(cursor):
-  headers = [x[0] for x in cursor.description]
-  data = cursor.fetchall()
-  res = {}
-
-  for item in data:
-    res = (dict(zip(headers, item)))
-
-  return res
 
 if __name__ == '__main__':
     app.run()
